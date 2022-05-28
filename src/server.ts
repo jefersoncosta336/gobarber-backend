19cad6789a8d85cd6159734023a92039301a46bf
jefersonwork <jefersoncosta336@gmail.com>
2022-05-28 20:22:58 -0300

    Validade appointment in same date

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import routes from './routes'
 
 const app = express()
 
-app.use(bodyParser.json(), routes)
+app.use(express.json())
+app.use(routes)
 
 app.listen(3333, () => {
   console.log('Servindo')
